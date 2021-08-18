7506a6a8bc7509b53a150403c06d852390b541c5
Paul Frazee <pfrazee@gmail.com>
2021-08-18 13:37:32 -0500

    Switch to blank d.ts files for now

--- build.js
+++ build.js
@@ -24,6 +24,7 @@ build({
     NodeModulesPolyfills.NodeModulesPolyfillPlugin()
   ],
 })
+fs.writeFileSync(`dist/hyperspace-client.${hcPkg.version}.build.d.ts`, `declare module 'hyperspace-client.${hcPkg.version}.build.js' {}`)
 build({
   format: 'esm',
   entryPoints: ['hyperbee/index.js'],
@@ -39,6 +40,12 @@ build({
     NodeModulesPolyfills.NodeModulesPolyfillPlugin()
   ],
 })
+fs.writeFileSync(`dist/hyperbee.${hbeePkg.version}.build.d.ts`, `declare module 'hyperbee.${hbeePkg.version}.build.js' {}`)
+
+/*
+TODO: If we want to generate meaningful d.ts files, this is a good start
+      It's not great, however, as you can see by the AST manipulation occurring here
+      for now, we just generate empty modules
 
 if (sh.exec('./node_modules/.bin/tsc hyperspace-client/index.js --declaration --allowJs --emitDeclarationOnly --outdir types').code !== 0) {
   throw 'Failed to generate hyperspace-client d.ts'
@@ -50,13 +57,32 @@ transformDTS('types/index.d.ts', ast => {
   HyperspaceClientCls.getMethod('status').getParameter('cb').setHasQuestionToken(true)
   HyperspaceClientCls.getMethod('stop').getParameter('cb').setHasQuestionToken(true)
   HyperspaceClientCls.getMethod('ready').getParameter('cb').setHasQuestionToken(true)
+  const RemoteCorestoreCls = ast.getClass('RemoteCorestore')
+  RemoteCorestoreCls.getMethod('get').getParameter('key').setHasQuestionToken(true)
+  RemoteCorestoreCls.getMethod('ready').getParameter('cb').setHasQuestionToken(true)
+  RemoteCorestoreCls.getMethod('close').getParameter('cb').setHasQuestionToken(true)
+  const RemoteNetworkerCls = ast.getClass('RemoteNetworker')
+  RemoteNetworkerCls.getMethod('ready').getParameter('cb').setHasQuestionToken(true)
+  RemoteNetworkerCls.getMethod('configure').getParameter('cb').setHasQuestionToken(true)
+  RemoteNetworkerCls.getMethod('status').getParameter('cb').setHasQuestionToken(true)
+  RemoteNetworkerCls.getMethod('allStatuses').getParameter('cb').setHasQuestionToken(true)
 })
 sh.mv('types/index.d.ts', `dist/hyperspace-client.${hcPkg.version}.build.d.ts`)
-if (sh.exec('./node_modules/.bin/tsc hyperbee/index.js --declaration --allowJs --emitDeclarationOnly --outdir types').code !== 0) {
+
+if (sh.exec('./node_modules/.bin/tsc hyperbee/index.js hyperbee/iterators/range.js --declaration --allowJs --emitDeclarationOnly --outdir types').code !== 0) {
   throw 'Failed to generate hyperbee d.ts'
 }
+transformDTS('types/index.d.ts', ast => {
+  const HyperBeeCls = ast.getClass('HyperBee')
+  HyperBeeCls.getMethod('createReadStream').getParameter('opts').setHasQuestionToken(true)
+  HyperBeeCls.getMethod('createHistoryStream').getParameter('opts').setHasQuestionToken(true)
+  HyperBeeCls.getMethod('createDiffStream').getParameter('opts').setHasQuestionToken(true)
+  HyperBeeCls.getMethod('get').getParameter('opts').setHasQuestionToken(true)
+  HyperBeeCls.getMethod('put').getParameter('opts').setHasQuestionToken(true)
+  HyperBeeCls.getMethod('del').getParameter('opts').setHasQuestionToken(true)
+})
 sh.mv('types/index.d.ts', `dist/hyperbee.${hbeePkg.version}.build.d.ts`)
-sh.rm('-rf', 'types')
+// sh.rm('-rf', 'types')
 
 function transformDTS (path, fn) {
   const project = new Project({
@@ -77,3 +103,4 @@ function transformDTS (path, fn) {
   ast.saveSync()
   fs.writeFileSync(path, project.getFileSystem().readFileSync('/index.d.ts'), 'utf8')
 }
+*/
