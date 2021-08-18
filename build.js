067fb47482424aea140075d0685d153c10142ad8
Paul Frazee <pfrazee@gmail.com>
2021-08-18 13:19:38 -0500

    Add automated d.ts corrections

--- build.js
+++ build.js
@@ -1,3 +1,4 @@
+import { Project, ScriptTarget } from 'ts-morph'
 import GlobalsPolyfills from '@esbuild-plugins/node-globals-polyfill'
 import NodeModulesPolyfills from '@esbuild-plugins/node-modules-polyfill'
 import { build } from 'esbuild'
@@ -42,9 +43,37 @@ build({
 if (sh.exec('./node_modules/.bin/tsc hyperspace-client/index.js --declaration --allowJs --emitDeclarationOnly --outdir types').code !== 0) {
   throw 'Failed to generate hyperspace-client d.ts'
 }
+transformDTS('types/index.d.ts', ast => {
+  const HyperspaceClientCls = ast.getClass('HyperspaceClient')
+  HyperspaceClientCls.getProperty('corestore').setType('(name?: any) => any')
+  HyperspaceClientCls.getProperty('replicate').setType('(core?: any, cb?: any) => any')
+  HyperspaceClientCls.getMethod('status').getParameter('cb').setHasQuestionToken(true)
+  HyperspaceClientCls.getMethod('stop').getParameter('cb').setHasQuestionToken(true)
+  HyperspaceClientCls.getMethod('ready').getParameter('cb').setHasQuestionToken(true)
+})
 sh.mv('types/index.d.ts', `dist/hyperspace-client.${hcPkg.version}.build.d.ts`)
 if (sh.exec('./node_modules/.bin/tsc hyperbee/index.js --declaration --allowJs --emitDeclarationOnly --outdir types').code !== 0) {
   throw 'Failed to generate hyperbee d.ts'
 }
 sh.mv('types/index.d.ts', `dist/hyperbee.${hbeePkg.version}.build.d.ts`)
 sh.rm('-rf', 'types')
+
+function transformDTS (path, fn) {
+  const project = new Project({
+    useInMemoryFileSystem: true,
+    compilerOptions: {
+      target: ScriptTarget.Latest
+    },
+    manipulationSettings: {
+      indentationText: '  ',
+      useTrailingCommas: true,
+      insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces: true,
+    }
+  })
+  const ast = project.createSourceFile(`/index.d.ts`, fs.readFileSync(path, 'utf8'))
+
+  fn(ast)
+
+  ast.saveSync()
+  fs.writeFileSync(path, project.getFileSystem().readFileSync('/index.d.ts'), 'utf8')
+}
