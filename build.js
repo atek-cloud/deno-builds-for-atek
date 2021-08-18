67ae149702532522b67282a37b6b286a163a6293
Paul Frazee <pfrazee@gmail.com>
2021-08-18 12:48:32 -0500

    Add d.ts file generation

--- build.js
+++ build.js
@@ -3,6 +3,7 @@ import NodeModulesPolyfills from '@esbuild-plugins/node-modules-polyfill'
 import { build } from 'esbuild'
 import fs from 'fs'
 import path from 'path'
+import sh from 'shelljs'
 
 const hcPkg = JSON.parse(fs.readFileSync(path.join('hyperspace-client', 'package.json'), 'utf8'))
 const hbeePkg = JSON.parse(fs.readFileSync(path.join('hyperbee', 'package.json'), 'utf8'))
@@ -27,7 +28,7 @@ build({
   entryPoints: ['hyperbee/index.js'],
   bundle: true,
   platform: 'node',
-  outfile: `dist/hyperbee.${hcPkg.version}.build.js`,
+  outfile: `dist/hyperbee.${hbeePkg.version}.build.js`,
   banner: {js: 'const global = {}\n'}, // for some reason this doesn't get done and we need it
   plugins: [
     GlobalsPolyfills.NodeGlobalsPolyfillPlugin({
@@ -37,3 +38,13 @@ build({
     NodeModulesPolyfills.NodeModulesPolyfillPlugin()
   ],
 })
+
+if (sh.exec('./node_modules/.bin/tsc hyperspace-client/index.js --declaration --allowJs --emitDeclarationOnly --outdir types').code !== 0) {
+  throw 'Failed to generate hyperspace-client d.ts'
+}
+sh.mv('types/index.d.ts', `dist/hyperspace-client.${hcPkg.version}.build.d.ts`)
+if (sh.exec('./node_modules/.bin/tsc hyperbee/index.js --declaration --allowJs --emitDeclarationOnly --outdir types').code !== 0) {
+  throw 'Failed to generate hyperbee d.ts'
+}
+sh.mv('types/index.d.ts', `dist/hyperbee.${hbeePkg.version}.build.d.ts`)
+sh.rm('-rf', 'types')
